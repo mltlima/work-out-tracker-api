9151ec5a27d0fc0bbf4ef64fa9f6b03eca9532e3
mltlima <miguellt@dcc.ufrj.br>
2022-08-06 00:34:15 -0300

    error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import dotenv from 'dotenv';
 import "express-async-errors";
 
 import router from "./routers/index.js";
-//import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use(router);
-//app.use(errorHandler)
+app.use(router);
+app.use(errorHandlerMiddleware)
 
 const PORT = process.env.PORT || 5000;
 
---
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError, errorTypeToStatusCode, isAppError } from "../utils/errorUtils.js";
+
+export function errorHandlerMiddleware(
+  error: Error | AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(error);
+
+  if (isAppError(error)) {
+    return res.status(errorTypeToStatusCode(error.type)).send(error.message);
+  }
+
+  return res.sendStatus(500);
+}
